d49e2b08d547c2e9a8e42938ffbe89c3a7f0b5a8
Abdull Baten <abdulbatinchy@gmail.com>
2026-09-04 09:21:12 +0600

    initialised project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <section>
+        <h3 className="text-4xl text-center">
+          hellow world, this is abdul baten
+        </h3>
+      </section>
+    </>
+  );
+}
+
+export default App;
